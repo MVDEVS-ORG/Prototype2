b596935c07600a60da21b9c4f908c4a590195614
Mohit Chaudhary <66890438+MohitChaudhary2@users.noreply.github.com>
2025-05-27 16:37:45 +0530

    [FTM-26] Created Dialogue System
    Also Moved Scripts to the correct locations

---
+++ Prototype-2/Assets/Prototype2/Scripts/DialogManager/DialogueManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Prototype2.Scripts
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        public TMP_Text speakerText;
+        public TMP_Text dialogueText;
+        public Button nextButton;
+        public Transform dialogueManagerUI;
+
+        private Queue<IDialogueLine> dialogueLines;
+        private IDialogueProvider dialogueProvider;
+
+        void Start()
+        {
+            dialogueLines = new Queue<IDialogueLine>();
+            nextButton.onClick.AddListener(DisplayNextLine);
+            dialogueManagerUI.gameObject.SetActive(false);
+        }
+
+        public void StartConversation()
+        {
+            dialogueManagerUI.gameObject.SetActive(true);
+            dialogueProvider = new JsonDialogueProvider("dialogues");
+            StartDialogue("intro");
+        }
+        public void StartDialogue(string conversationId)
+        {
+            IConversation conversation = dialogueProvider.GetConversationById(conversationId);
+
+            if (conversation != null)
+            {
+                dialogueLines.Clear();
+                foreach (var line in conversation.Lines)
+                {
+                    dialogueLines.Enqueue(line);
+                }
+
+                DisplayNextLine();
+            }
+            else
+            {
+                Debug.LogWarning($"Conversation ID {conversationId} not found.");
+            }
+        }
+
+        public void DisplayNextLine()
+        {
+            if (dialogueLines.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+
+            IDialogueLine line = dialogueLines.Dequeue();
+            speakerText.text = line.Speaker;
+            dialogueText.text = line.Text;
+        }
+
+        void EndDialogue()
+        {
+            speakerText.text = "";
+            dialogueText.text = "";
+            Debug.Log("Dialogue ended.");
+            dialogueManagerUI.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Prototype-2/Assets/Prototype2/Scripts/DialogManager/JsonDialogueProvider.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Prototype2.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Prototype2.Scripts
+{
+    public class JsonDialogueProvider : IDialogueProvider
+    {
+        private List<IConversation> conversations;
+
+        public JsonDialogueProvider(string fileName)
+        {
+            TextAsset jsonFile = Resources.Load<TextAsset>(fileName);
+            DialogueDataJson data = JsonUtility.FromJson<DialogueDataJson>(jsonFile.text);
+            conversations = new List<IConversation>(data.conversations);
+        }
+
+        public List<IConversation> GetAllConversations()
+        {
+            return conversations;
+        }
+
+        public IConversation GetConversationById(string id)
+        {
+            return conversations.Find(c => c.Id == id);
+        }
+    }
+}
